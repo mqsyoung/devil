bbbcc4d0a3bd40090c001bc1e19fc33d790fa76c
mengqs-a <mqsyoung123!>
2019-08-08 09:25:51 +0800

    添加日志

--- src/main/java/com/devil/DevilApplication.java
+++ src/main/java/com/devil/DevilApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+@EnableEurekaServer  // 注册中心注解
 @SpringBootApplication
 public class DevilApplication {
 
