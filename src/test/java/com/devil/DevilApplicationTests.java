c7e19cff8082beaa960f2e944d06579004485283
mengqs-a <mqsyoung123!>
2019-08-08 09:17:05 +0800

    注册中心

---
+++ src/test/java/com/devil/DevilApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevilApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
